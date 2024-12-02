a87b02bc256b062b590fb3b88c5154745282a2d2
ssalinaa <salina.chinarska29@gmail.com>
2024-12-02 20:22:12 +0200

    Linked List Tasks

--- LinkedList/MergeTwoSortedLists.cpp
+++ LinkedList/MergeTwoSortedLists.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+struct ListNode {
+    int value;
+    ListNode* next;
+    ListNode() : value(0), next(nullptr) { }
+    ListNode(int value) : value(value), next(nullptr) { }
+    ListNode(int value, ListNode* next) : value(value), next(next) { }
+};
+
 int main() {
 
     return 0;
