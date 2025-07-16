c6e2f3ae16dae1f14a85456db5876a267d9f472a
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2025-07-16 11:09:25 +0300

    Recursive search of an element

--- SinglyLinkedList/searchAnElementRecursive.cpp
+++ SinglyLinkedList/searchAnElementRecursive.cpp
@@ -10,3 +10,27 @@ struct Node {
     }
 };
 
+bool isPresentRecursive(Node* head, int key) {
+    if(!head) return false;
+    if(head->value == key) return true;
+
+    return isPresentRecursive(head->next, key);
+}
+
+int main() {
+
+    Node* head = new Node(14);
+    head->next = new Node(15);
+    head->next->next = new Node(21);
+    head->next->next->next = new Node(13);
+    head->next->next->next->next = new Node(30);
+    head->next->next->next->next->next = new Node(10);
+    int key = 13;
+    if(isPresentRecursive(head, key)) {
+        std::cout << "Yes" << std::endl;
+    } else {
+        std::cout << "No" << std::endl;
+    }
+
+    return 0;
+}
